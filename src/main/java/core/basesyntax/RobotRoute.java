59e3390586e1143b961c3c4ba7cbe8f8f5825333
Dmytro <sibertdom.wot@gmail.com>
2025-12-01 22:55:45 +0200

    SibertRobot

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,34 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        moveByAxis(robot, toX, robot.getX(), Direction.RIGHT, Direction.LEFT);
+
+        moveByAxis(robot, toY, robot.getY(), Direction.UP, Direction.DOWN);
+    }
+
+    private void moveByAxis(Robot robot, int targetCoordinate, int currentCoordinate,
+                            Direction positiveDirection, Direction negativeDirection) {
+
+        int distance = targetCoordinate - currentCoordinate;
+
+        if (distance > 0) {
+            turnTo(robot, positiveDirection);
+            performSteps(robot, distance);
+        } else if (distance < 0) {
+            turnTo(robot, negativeDirection);
+            performSteps(robot, Math.abs(distance));
+        }
+    }
+
+    private void turnTo(Robot robot, Direction targetDirection) {
+        while (robot.getDirection() != targetDirection) {
+            robot.turnLeft();
+        }
+    }
+    
+    private void performSteps(Robot robot, int steps) {
+        for (int i = 0; i < steps; i++) {
+            robot.stepForward();
+        }
     }
 }
